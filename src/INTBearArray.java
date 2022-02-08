f24d25cb1c0acf3876ac6d0d422ad6a4e70a3567
Charles Lett Jr <cclett2000@gmail.com>
2022-02-08 11:34:08 -0600

    Removed unneeded 'this.var' statements

--- src/INTBearArray.java
+++ src/INTBearArray.java
@@ -5,12 +5,11 @@ import java.util.Random;
 
 public class INTBearArray {
     private int[] array = new int[0];                      // array init, can only be accessed from within here
-    private int[] temp;                                    // temp array init
     private final int demo_size = (920 / 419) + 22;        // number of values for demo
 
     // retrieve value from array
     public int getValue(int index){
-        return this.array[index];
+        return array[index];
     }
 
     // calculate the sum of the list
@@ -29,18 +28,19 @@ public class INTBearArray {
 
     // appends to array, increasing size by 1
     public void append(int value){
-        this.temp = new int[array.length + 1];                          // temp array; stores array data + empty slot for append
+        // temp array init
+        int[] temp = new int[array.length + 1];                         // temp array; stores array data + empty slot for append
 
         //System.arraycopy(array, 0, temp, 0, array.length);            // this works too, but I wanted to get it working manually
         for (int i = 0; i < array.length; i++) temp[i] = array[i];      // manual array copy
 
-        this.temp[temp.length-1] = value;                               // appends value to end of array
-        this.array = this.temp;                                         // modifies actual array
+        temp[temp.length-1] = value;                                    // appends value to end of array
+        array = temp;                                                   // modifies actual array
     }
 
     // prints array data
     public void print(){
-        System.out.println(Arrays.toString(this.array));
+        System.out.println(Arrays.toString(array));
     }
 
     // test/demonstrate array functionality
@@ -48,14 +48,14 @@ public class INTBearArray {
         Random rand = new Random();                                                 // init for RNG
         for (int i = 0; i < demo_size; i++) append(rand.nextInt(1000));
 
-        System.out.println(Arrays.toString(this.array));                            // print demo
+        System.out.println(Arrays.toString(array));                                 // print demo
     }
 
     // array stats
     public void stats(){
-        System.out.print("\nArray Length: " + this.array.length
-                       + "\nDemo Size: " + this.demo_size
-                       + "\nAvail. Indices: " + 0 + " - " + (this.array.length - 1)
+        System.out.print("\nArray Length: " + array.length
+                       + "\nDemo Size: " + demo_size
+                       + "\nAvail. Indices: " + 0 + " - " + (array.length - 1)
                        + "\n");
     }
 
